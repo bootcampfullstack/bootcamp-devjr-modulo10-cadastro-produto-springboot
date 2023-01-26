da1d8c45920ad0b1363ad75a79e9902cd424e83b
Glauco Todesco <glauco.todesco@abutua.com>
2023-01-26 20:53:44 -0300

    UpdateProduct

--- src/main/java/com/abutua/productbackend/resources/ProductController.java
+++ src/main/java/com/abutua/productbackend/resources/ProductController.java
@@ -12,12 +12,15 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
+import com.abutua.productbackend.models.Category;
 import com.abutua.productbackend.models.Product;
+import com.abutua.productbackend.repositories.CategoryRepository;
 import com.abutua.productbackend.repositories.ProductRepository;
 
 @RestController
@@ -26,6 +29,9 @@ public class ProductController {
 
     @Autowired
     private ProductRepository productRepository;
+
+    @Autowired
+    private CategoryRepository categoryRepository;
 
     @PostMapping("products")
     public ResponseEntity<Product> save(@RequestBody Product product) {
@@ -67,6 +73,31 @@ public class ProductController {
     }
     
 
+    @PutMapping("products/{id}")    
+    public ResponseEntity<Void> updateProduct(@PathVariable int id, @RequestBody Product productUpdate) {
+        
+        Product product = productRepository.findById(id)
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
+
+        if(productUpdate.getCategory() == null){
+           throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category can not be empty");
+        }
+
+        
+        Category category = categoryRepository.findById(productUpdate.getCategory().getId())
+                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
+
+        product.setDescription(productUpdate.getDescription());
+        product.setName(productUpdate.getName());
+        product.setPrice(productUpdate.getPrice());
+        product.setNewProduct(productUpdate.isNewProduct());
+        product.setPromotion(productUpdate.isPromotion());
+        product.setCategory(category);
+
+        productRepository.save(product);
+                
+        return ResponseEntity.ok().build(); 
+    }
     
 
 }

6d75f69e1ae162cb506e4380027c9b0064f49308
Glauco Todesco <glauco.todesco@abutua.com>
2023-01-27 19:46:47 -0300

    Add Services

--- src/main/java/com/abutua/productbackend/resources/ProductController.java
+++ src/main/java/com/abutua/productbackend/resources/ProductController.java
@@ -4,7 +4,6 @@ import java.net.URI;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
@@ -14,28 +13,21 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.server.ResponseStatusException;
 import org.springframework.web.servlet.support.ServletUriComponentsBuilder;
 
-import com.abutua.productbackend.models.Category;
-import com.abutua.productbackend.models.Product;
-import com.abutua.productbackend.repositories.CategoryRepository;
-import com.abutua.productbackend.repositories.ProductRepository;
+ import com.abutua.productbackend.models.Product;
+import com.abutua.productbackend.services.ProductService;
 
 @RestController
 @CrossOrigin
 public class ProductController {
 
     @Autowired
-    private ProductRepository productRepository;
-
-    @Autowired
-    private CategoryRepository categoryRepository;
+    private ProductService productService;
 
     @PostMapping("products")
     public ResponseEntity<Product> save(@RequestBody Product product) {
-       
-        product = productRepository.save(product);
+        product = productService.save(product);
 
         URI location = ServletUriComponentsBuilder
                 .fromCurrentRequest()
@@ -44,57 +36,28 @@ public class ProductController {
                 .toUri();
  
         return ResponseEntity.created(location).body(product);
-
     }
 
     @GetMapping("products/{id}")
     public ResponseEntity<Product> getProduct(@PathVariable int id) {
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
-
+        Product product = productService.getById(id);
         return ResponseEntity.ok(product);
     }
 
     @GetMapping("products")  
     public List<Product> getProducts() { 
-        return productRepository.findAll();
+        return productService.getAll();
     }
 
     @DeleteMapping("products/{id}")    
     public ResponseEntity<Void> removeProduct(@PathVariable int id) {
-        
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
-
-        productRepository.delete(product);
-                
+        productService.deleteById(id);                
         return ResponseEntity.noContent().build(); 
     }
-    
-
+   
     @PutMapping("products/{id}")    
     public ResponseEntity<Void> updateProduct(@PathVariable int id, @RequestBody Product productUpdate) {
-        
-        Product product = productRepository.findById(id)
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Product not found"));
-
-        if(productUpdate.getCategory() == null){
-           throw new ResponseStatusException(HttpStatus.BAD_REQUEST, "Category can not be empty");
-        }
-
-        
-        Category category = categoryRepository.findById(productUpdate.getCategory().getId())
-                .orElseThrow(() -> new ResponseStatusException(HttpStatus.NOT_FOUND, "Category not found"));
-
-        product.setDescription(productUpdate.getDescription());
-        product.setName(productUpdate.getName());
-        product.setPrice(productUpdate.getPrice());
-        product.setNewProduct(productUpdate.isNewProduct());
-        product.setPromotion(productUpdate.isPromotion());
-        product.setCategory(category);
-
-        productRepository.save(product);
-                
+        productService.update(id, productUpdate);
         return ResponseEntity.ok().build(); 
     }
     
